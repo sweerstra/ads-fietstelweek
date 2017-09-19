4464c5b804e003c1add06803055ef02423d55fb9
Sjoerd <s.weerstra@student.fontys>
2017-09-19 13:41:32 +0200

    Sidebar view

--- fietsenstallingen/script.js
+++ fietsenstallingen/script.js
@@ -1,6 +1,7 @@
 (function () {
     const CityInput = document.getElementById('city');
     const NotFoundLabel = document.getElementById('not-found');
+    const Sidebar = document.getElementById('sidebar');
 
     const map = new OpenLayers.Map('map');
     const graphic = 'http://bl.ocks.org/KoGor/raw/81e6ce8945c43405e3e9/9b91f2a958c139a1be50a4cd93bb6d9646bc7b56/bike.svg';
@@ -8,7 +9,14 @@
     map.addLayer(new OpenLayers.Layer.OSM());
     const epsg4326 = new OpenLayers.Projection('EPSG:4326');
     const projectTo = map.getProjectionObject();
-    const vectorLayer = new OpenLayers.Layer.Vector('Overlay');
+    const vectorLayer = new OpenLayers.Layer.Vector('Overlay', {
+        eventListeners: {
+            featureclick(e) {
+                const { stalling } = e.feature.data;
+                Sidebar.innerHTML = Object.keys(stalling).map(key => key + ': ' + stalling[key] + '<br/>');
+            }
+        }
+    });
 
     document.addEventListener('DOMContentLoaded', getFietsenstallingen);
     document.getElementById('search').addEventListener('click', getFietsenstallingen);
@@ -21,16 +29,14 @@
     });
 
     function getFietsenstallingen() {
-        // Tilburg: lon 5.0808, lat 51.5590
-
         fetch('./data.json')
             .then(data => data.json())
             .then((json) => {
-                const cityValue = CityInput.value.toLowerCase();
-                if (!cityValue) return;
+                const value = CityInput.value.toLowerCase();
+                if (!value) return;
 
                 const fietsenstalling = json.Fietsenstallingen.Fietsenstalling;
-                const stallingenInCity = fietsenstalling.filter(f => f.Plaats.__cdata.toLowerCase() === cityValue);
+                const stallingenInCity = fietsenstalling.filter(f => f.Plaats.__cdata.toLowerCase() === value);
 
                 NotFoundLabel.style.display = stallingenInCity.length ? 'none' : 'inline';
                 if (stallingenInCity.length === 0) return;
@@ -46,13 +52,12 @@
 
                     const feature = new OpenLayers.Feature.Vector(
                         new OpenLayers.Geometry.Point(lon, lat).transform(epsg4326, projectTo),
-                        { description: 'Marker number ' + index },
+                        { index, stalling: formatStalling(stallingenInCity[index]) },
                         {
                             externalGraphic: graphic,
                             graphicHeight: 25,
                             graphicWidth: 25,
-                            graphicXOffset: -12,
-                            graphicYOffset: -25
+                            cursor: 'pointer'
                         });
 
                     vectorLayer.addFeatures(feature);
@@ -65,5 +70,27 @@
 
                 map.addLayer(vectorLayer);
             });
+    }
+
+    function formatStalling(data) {
+        return {
+            naam: data.Naam.__cdata,
+            omschrijving: data.Omschrijving ? data.Omschrijving.__cdata : null,
+            gemeente: data.Gemeente.__cdata,
+            straat: data.Straat.__cdata,
+            postcode: data.Postcode.__cdata,
+            plaats: data.Plaats.__cdata,
+            url: data.Url,
+            toegang: data.Toegangscontrole ? data.Toegangscontrole.__cdata : null,
+            capaciteit: data.CapaciteitTotaal,
+            openingstijden: data.Openingstijden,
+            isStationStalling: data.Stationsstalling.toLowerCase() === 'ja',
+            type: data.Type,
+            bewaakt: data.Type.toLowerCase() === 'bewaakt',
+            beheerder: {
+                naam: data.Beheerder.__cdata,
+                contact: data.BeheerderContact.__cdata
+            }
+        };
     }
 })()

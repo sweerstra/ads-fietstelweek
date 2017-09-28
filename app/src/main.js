146e735a2645811a1efd0b28027bb3ed21dfed58
Rob <robvangastel@hotmail.com>
2017-09-28 11:25:57 +0200

    Add vuetify plugin

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+import '../node_modules/vuetify/dist/vuetify.min.css';
 import VueOpenLayers from './vendor/vue-openlayers';
 import '../node_modules/openlayers/css/ol.css';
 import App from './App';
 import router from './router';
 
 Vue.use(VueOpenLayers);
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 

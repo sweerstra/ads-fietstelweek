3af6d448093b598abd4db6766e234e8664113dac
Sjoerd <s.weerstra@student.fontys>
2017-11-22 15:30:28 +0100

    Sugar synax

--- poc/script.js
+++ poc/script.js
@@ -3,19 +3,15 @@ const extent = [-285401.92, 22598.08, 595401.9199999999, 903401.9199999999];
 const resolutions = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420];
 const projection = new ol.proj.Projection({ code: 'EPSG:28992', units: 'm', extent });
 
-const tileUrlFunction = function ([z, y, x]) {
-    return y < 0 || x < 0 ? '' : `${url}${z}/${y}/${x}.png`;
-};
+const tileUrlFunction = ([z, y, x]) => y < 0 || x < 0 ? '' : `${url}${z}/${y}/${x}.png`;
 
 const styles = {
     MultiLineString: new ol.style.Style({
-        stroke: new ol.style.Stroke({ color: 'purple', width: 2 })
+        stroke: new ol.style.Stroke({ color: '#F44336', width: 2 })
     }),
 };
 
-const styleFunction = function (feature) {
-    return styles[feature.getGeometry().getType()];
-};
+const styleFunction = feature => styles[feature.getGeometry().getType()];
 
 const vectorLayer = new ol.layer.Vector({
     source: new ol.source.Vector({
@@ -48,7 +44,7 @@ const map = new ol.Map({
         minZoom: 3,
         maxZoom: 13,
         projection,
-        center: [150000, 450000],
-        zoom: 3
+        center: [150000, 400000],
+        zoom: 5
     })
 });

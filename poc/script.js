146a8bb4c19726ea2ba705cc9ca902ff1467cbb7
Sjoerd <s.weerstra@student.fontys>
2017-11-30 11:55:07 +0100

    Separated layers

--- poc/script.js
+++ poc/script.js
@@ -3,9 +3,12 @@ const extent = [-285401.92, 22598.08, 595401.9199999999, 903401.9199999999];
 const resolutions = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420];
 const projection = new ol.proj.Projection({ code: 'EPSG:28992', units: 'm', extent });
 
-const tileUrlFunction = ([z, y, x]) => y < 0 || x < 0 ? '' : `${url}${z}/${y}/${x}.png`;
+const tileUrlFunction = ([z, x, y]) => y < 0 || x < 0 ? '' : `${url}${z}/${x}/${y}.png`;
 
 const styles = {
+    fill: new ol.style.Fill({
+        color: 'red'
+    }),
     MultiLineString: new ol.style.Style({
         stroke: new ol.style.Stroke({ color: '#F44336', width: 2 })
     }),
@@ -13,7 +16,18 @@ const styles = {
 
 const styleFunction = feature => styles[feature.getGeometry().getType()];
 
-const vectorLayer = new ol.layer.Vector({
+const baseLayer = new ol.layer.Tile({
+    source: new ol.source.TileImage({
+        projection,
+        tileGrid: new ol.tilegrid.TileGrid({
+            origin: [-285401.92, 22598.08],
+            resolutions
+        }),
+        tileUrlFunction
+    })
+});
+
+const streetLayer = new ol.layer.Vector({
     source: new ol.source.Vector({
         features: (new ol.format.GeoJSON()).readFeatures(DATA)
     }),
@@ -22,26 +36,9 @@ const vectorLayer = new ol.layer.Vector({
 
 const map = new ol.Map({
     target: 'map',
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.TileImage({
-                attributions: [
-                    new ol.Attribution({
-                        html: 'Kaartgegevens: © <a href="http://www.cbs.nl">CBS</a>, <a href="http://www.kadaster.nl">Kadaster</a>, <a href="http://openstreetmap.org">OpenStreetMap</a><span class="printhide">-auteurs (<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>).</span>'
-                    })
-                ],
-                projection,
-                tileGrid: new ol.tilegrid.TileGrid({
-                    origin: [-285401.92, 22598.08],
-                    resolutions
-                }),
-                tileUrlFunction
-            })
-        }),
-        vectorLayer
-    ],
+    layers: [baseLayer, streetLayer],
     view: new ol.View({
-        minZoom: 3,
+        minZoom: 0,
         maxZoom: 13,
         projection,
         center: [150000, 400000],

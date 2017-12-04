011bc49cd7eb440a36cf9821bc8cd9c1a56c3a4e
Rob <robvangastel@hotmail.com>
2017-12-04 16:21:11 +0100

    Add resize

--- app/src/components/Map.vue
+++ app/src/components/Map.vue
@@ -17,10 +17,11 @@
     created() {
       this.$nextTick(() => {
         this.drawMap();
-      });
 
-      EventBus.$on('resize', () => {
-        this.map.invalidateSize();
+        EventBus.$on('resize', () => {
+          console.log('resize')
+          this.map.invalidateSize();
+        });
       });
     },
     methods: {

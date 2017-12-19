265ee710eb3f96361ebecc257646a4c874bbcc10
Rob <robvangastel@hotmail.com>
2017-12-19 12:52:51 +0100

    Update card

--- app/src/components/Research.vue
+++ app/src/components/Research.vue
@@ -1,22 +1,40 @@
 <template>
-  <v-layout row wrap>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <v-card-media src="/static/fiets-header.jpg" height="200px">
-        </v-card-media>
-        <v-container fill-height fluid>
-          <v-layout row fill-height>
-            <v-flex xs12 align-end flexbox>
-              <div>
-                <div class="headline">What is een snelfietsroute?</div>
-                <div>Nederland is bij uitstek een land dat graag fietst. Verschillende cijfers bevestigen dit.</div>
-              </div>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container fluid row grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs12 sm6>
+        <v-card>
+          <v-card-media src="/static/fiets-header.jpg" height="200px">
+          </v-card-media>
+          <v-container fill-height fluid>
+            <v-layout row fill-height>
+              <v-flex xs12 align-end flexbox>
+                <div>
+                  <div class="headline">What is een snelfietsroute?</div>
+                  <div>Nederland is bij uitstek een land dat graag fietst. Verschillende cijfers bevestigen dit.</div>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>    
+      <v-flex xs12 sm6>
+        <v-card>
+          <v-card-media src="/static/fiets-header.jpg" height="200px">
+          </v-card-media>
+          <v-container fill-height fluid>
+            <v-layout row fill-height>
+              <v-flex xs12 align-end flexbox>
+                <div>
+                  <div class="headline">What is een snelfietsroute?</div>
+                  <div>Nederland is bij uitstek een land dat graag fietst. Verschillende cijfers bevestigen dit.</div>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
